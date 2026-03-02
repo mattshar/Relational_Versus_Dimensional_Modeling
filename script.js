1fdf8560d1de5d0f69c37fa4b07d889725c9249f
Matthew <matthewsharpe49@icloud.com>
2026-03-01 20:14:16 -0500

    Trying out JQuery logic on top of Vanilla client-side JS

--- script.js
+++ script.js
@@ -44,4 +44,34 @@ document.querySelectorAll('.topic, .glossary-box, .intro-card').forEach(el => {
     observer.observe(el);
 });
 
+
+
 });
+
+
+// JQuery addition below  on top of vanilla JS 
+
+
+$(document).ready(function() {
+    // Progress Bar Logic
+    $(window).on('scroll', function() {
+        var winScroll = $(window).scrollTop();
+        var height = $(document).height() - $(window).height();
+        var scrolled = (winScroll / height) * 100;
+        $('#myBar').css('width', scrolled + '%');
+    });
+
+    // Scroll to Top Button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 300) {
+            $('#scrollTopBtn').fadeIn();
+        } else {
+            $('#scrollTopBtn').fadeOut();
+        }
+    });
+
+    $('#scrollTopBtn').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 600);
+    });
+});
+
